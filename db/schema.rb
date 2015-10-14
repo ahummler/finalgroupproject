6364205f8478ea09f3dae495dc121b642752d5b5
Arthur <phillysuperbowl@gmail.com>
2015-10-14 16:53:53 -0400

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151014201038) do
+ActiveRecord::Schema.define(version: 20151010000003) do
 
   create_table "business_cards", force: :cascade do |t|
     t.string   "company_name"
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 20151014201038) do
     t.string   "email"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-    t.integer  "user_id"
   end
 
   create_table "relationships_tables", force: :cascade do |t|
@@ -43,7 +42,6 @@ ActiveRecord::Schema.define(version: 20151014201038) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "phone"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
