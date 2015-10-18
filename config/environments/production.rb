ac559ded0cbbc65df177ad2f342f6bbf711eb8a4
Gabrielle Gerlitz <gabrielle.gerlitz@gmail.com>
2015-10-18 11:51:50 -0400

    added host for action mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { host: "cardvalet.herokuapp.com" }
   config.action_mailer.smtp_settings = {
       :address   => "smtp.mandrillapp.com",
       :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
